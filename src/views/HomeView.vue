27a790098b5461e25a25cb761d6f31ac8d420f85
Misael Barcelona <misael.barcelona@wpengine.com>
2024-11-04 13:08:05 -0500

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+
+</script>
+
+<template>
+    <Hero></Hero>
+    <HomeCards></HomeCards>
+    <JobListings :limit="3" :showButton="true"></JobListings>
+</template>
